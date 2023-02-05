761dc6d78d2ae0849e35c4abf9ef106e4d8fbf1d
RomansyahOfficial <ricky@hotelmurah.com>
2023-02-05 17:10:26 +0700

    Add pages login and register

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -6,16 +6,23 @@ class Auth extends CI_Controller {
 
 	public function login()
 	{
-		echo "login";
-        die;
-		$this->load->view('template/footer_home');
+		$this->load->view('auth/header_auth');
+		$this->load->view('auth/login');
+		$this->load->view('auth/footer_auth');
 	}
-
+	
 	public function register()
 	{
-		echo "Registration";
-        die;
-		$this->load->view('template/footer_home');
+		$this->load->view('auth/header_auth');
+		$this->load->view('auth/register');
+		$this->load->view('auth/footer_auth');
+	}
+
+	public function forgot_password()
+	{
+		$this->load->view('auth/header_auth');
+		$this->load->view('auth/forgot_password');
+		$this->load->view('auth/footer_auth');
 	}
     
 }
