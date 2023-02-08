b5927b2e8a11c812d81312efbf3d2efa6972de26
RomansyahOfficial <ricky@hotelmurah.com>
2023-02-08 18:09:44 +0700

    Update register sistem

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -92,6 +92,70 @@ class Auth extends CI_Controller {
 
 	}
 
+	public function proses_register()
+	{
+
+		$data = $this->input->post();
+
+		$first_name = $data['firstname'];
+		$last_name 	= $data['lastname'];
+		$email 		= $data['email'];
+		$password 	= $data['password'];
+
+		$post_field = [
+			"name" 	=> $first_name." ".$last_name,
+			"email" => $email,
+			"password" => $password
+		];
+
+		$post_field = json_encode($post_field);
+		$curl = curl_init();
+		$url = 'http://localhost:1315/api/v1/users/register';
+
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'POST',
+		CURLOPT_POSTFIELDS => $post_field,
+		CURLOPT_HTTPHEADER => array(
+			'Content-Type: application/json'
+		),
+		));
+
+		$response = curl_exec($curl);
+
+		
+		curl_close($curl);
+
+		$curl = curl_init();
+		$result = json_decode($response, true);
+
+		if($result['success'] == true){
+			$data_response = [
+				'message' => $result['message'],
+				'status' => $result['success'],
+				'id' => $result['data']['id'],
+				'name' => $result['data']['name'],
+				'email' => $result['data']['email'],
+				'token' => $result['data']['token']
+			];
+			echo json_encode($data_response);
+		}else{
+			$data_response = [
+				'message' => $result['message'],
+				'status' => $result['success'],
+				'error' => $result['error']
+			];
+			echo json_encode($data_response);
+		}
+
+	}
+
 	public function dashboard()
 	{
 		$data = $this->input->post();
