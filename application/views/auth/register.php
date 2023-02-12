9d31b30fc5610bd4657351a7d898349f5bca4f37
RomansyahOfficial <ricky@hotelmurah.com>
2023-02-12 17:19:27 +0700

    Register update margin

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -1,59 +1,62 @@
 <?php $this->load->view('auth/header_auth'); ?>
 
     <div class="container">
-
-        <div class="card o-hidden border-0 shadow-lg my-5">
-            <div class="card-body p-0">
-                <!-- Nested Row within Card Body -->
-                <div class="row">
-                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
-                    <div class="col-lg-7">
-                        <div class="p-5">
-                            <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
-                            </div>
-                            <form class="user">
-                                <div class="form-group row">
-                                    <div class="col-sm-6 mb-3 mb-sm-0">
-                                        <input type="text" class="form-control form-control-user" id="firstname" name="firstname"
-                                            placeholder="First Name">
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <input type="text" class="form-control form-control-user" id="lastname" name="lastname"
-                                            placeholder="Last Name">
-                                    </div>
+    <div class="row justify-content-center">
+        <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                        <div class="col-lg-7">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                                 </div>
-                                <div class="form-group">
-                                    <input type="email" class="form-control form-control-user" id="email"
-                                        placeholder="Email Address">
-                                </div>
-                                <div class="form-group row">
-                                    <div class="col-sm-12 mb-3 mb-sm-0">
-                                        <input type="password" class="form-control form-control-user"
-                                            id="password" name="password" placeholder="Password">
+                                <form class="user">
+                                    <div class="form-group row">
+                                        <div class="col-sm-6 mb-3 mb-sm-0">
+                                            <input type="text" class="form-control form-control-user" id="firstname" name="firstname"
+                                                placeholder="First Name">
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <input type="text" class="form-control form-control-user" id="lastname" name="lastname"
+                                                placeholder="Last Name">
+                                        </div>
                                     </div>
-                                    <!-- <div class="col-sm-6">
-                                        <input type="password" class="form-control form-control-user"
-                                            id="passwordRepeat" name="passwordRepeat" placeholder="Repeat Password">
-                                    </div> -->
-                                </div>
-                                <a name="btn_register" id="btn_register" onclick="verifikasi_data()" class="btn btn-primary btn-user btn-block">
-                                    Register Account
-                                </a>
+                                    <div class="form-group">
+                                        <input type="email" class="form-control form-control-user" id="email"
+                                            placeholder="Email Address">
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-sm-12 mb-3 mb-sm-0">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="password" name="password" placeholder="Password">
+                                        </div>
+                                        <!-- <div class="col-sm-6">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="passwordRepeat" name="passwordRepeat" placeholder="Repeat Password">
+                                        </div> -->
+                                    </div>
+                                    <a name="btn_register" id="btn_register" onclick="verifikasi_data()" class="btn btn-primary btn-user btn-block">
+                                        Register Account
+                                    </a>
+                                    <hr>
+                                </form>
                                 <hr>
-                            </form>
-                            <hr>
-                            <div class="text-center">
-                                <a class="small" href="<?= base_url('auth/forgot_password'); ?>">Forgot Password?</a>
-                            </div>
-                            <div class="text-center">
-                                <a class="small" href="<?= base_url('auth/login'); ?>">Already have an account? Login!</a>
+                                <div class="text-center">
+                                    <a class="small" href="<?= base_url('auth/forgot_password'); ?>">Forgot Password?</a>
+                                </div>
+                                <div class="text-center">
+                                    <a class="small" href="<?= base_url('auth/login'); ?>">Already have an account? Login!</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
     </div>
 
