4843eafc0e95d714e82fbd69a4ef188dfe85bbc3
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-14 06:56:55 +0700

    adding middleware

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,23 +6,18 @@ class Admin extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+
+		if ($this->session->has_userdata('id') == null) {
+            redirect('auth/login');
+        }
+
+        if ($this->session->userdata('role') == "user") {
+            redirect('user/dashboard');
+        }
 	}
 
     public function dashboard()
 	{
-		if (!isset($_SESSION))
-		{
-            session_start();
-		}
-
-		if (empty($_SESSION)) {
-            echo '<script type="text/javascript">';
-            echo 'window.location.href="'. base_url() .'auth/login";';
-            echo '</script>';
-			exit;
-        }
-
-        $data = $this->input->post();
 		$data['title'] = "Admin Dashboard";
 
 		$this->load->view('admin/dashboard/index', $data);
@@ -30,19 +25,6 @@ class Admin extends CI_Controller
 
     public function campaigns()
 	{
-		if (!isset($_SESSION))
-		{
-            session_start();
-		}
-
-		if (empty($_SESSION)) {
-            echo '<script type="text/javascript">';
-            echo 'window.location.href="'. base_url() .'auth/login";';
-            echo '</script>';
-			exit;
-        }
-
-        $data = $this->input->post();
 		$data['title'] = "Campaigns";
 
 		$this->load->view('admin/campaigns/index', $data);
