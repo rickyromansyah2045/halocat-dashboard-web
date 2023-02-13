7ddf8d55e74481ec8a6421cedaabc4a95a268c74
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-14 05:17:10 +0700

    tidy up code

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -13,13 +13,10 @@
                                 </div>
                                 <form class="user">
                                     <div class="form-group">
-                                        <input type="email" class="form-control form-control-user"
-                                            id="email" name="email" aria-describedby="emailHelp"
-                                            placeholder="Enter Email Address...">
+                                        <input type="email" class="form-control form-control-user" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter Email Address...">
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" class="form-control form-control-user"
-                                            id="password" name="password" placeholder="Password">
+                                        <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
                                     </div>
                                     <a name="btn_login" onclick="verifikasi_data()" id="btn_login" class="btn btn-primary btn-user btn-block">
                                         Login
