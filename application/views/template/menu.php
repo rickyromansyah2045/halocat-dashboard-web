db775266968a8f9df067688dcb006c0eda5b83ca
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-20 04:55:05 +0700

    add link to navbar brand

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
 	<div class="container">
-		<a class="navbar-brand" href="#">
+		<a class="navbar-brand" href="<?= site_url(); ?>">
 			<img class="logo_brand" src="<?= base_url('assets/img/logo_cloud2.png')?>">
 		</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,20 +8,20 @@
 		</button>
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav mr-auto">
-				<a class="nav-link nav_link_menu" href="<?= base_url(); ?>">Home</a>
-				<a class="nav-link nav_link_menu" href="<?= base_url('donations'); ?>">List of Donations</a>
-				<a class="nav-link nav_link_menu" href="<?= base_url('about'); ?>">About Us</a>
+				<a class="nav-link nav_link_menu" href="<?= site_url(); ?>">Home</a>
+				<a class="nav-link nav_link_menu" href="<?= site_url('donations'); ?>">List of Donations</a>
+				<a class="nav-link nav_link_menu" href="<?= site_url('about'); ?>">About Us</a>
 			</ul>
 			<ul class="navbar-nav mr0">
 				<?php if ($this->session->has_userdata('id')): ?>
 					<?php if ($this->session->userdata('role') == "user"): ?>
-						<a class="nav-link nav_link_menu" href="<?= base_url('user/dashboard'); ?>" style="margin-right: 0; padding-right: 0;">User Area</a>
+						<a class="nav-link nav_link_menu" href="<?= site_url('user/dashboard'); ?>" style="margin-right: 0; padding-right: 0;">User Area</a>
 					<?php else: ?>
-						<a class="nav-link nav_link_menu" href="<?= base_url('admin/dashboard'); ?>" style="margin-right: 0; padding-right: 0;">Admin Area</a>
+						<a class="nav-link nav_link_menu" href="<?= site_url('admin/dashboard'); ?>" style="margin-right: 0; padding-right: 0;">Admin Area</a>
 					<?php endif; ?>
 				<?php else: ?>
-					<a class="nav-link nav_link_menu" href="<?= base_url('auth/register'); ?>">Register</a>
-					<a class="nav-link nav_link_menu" href="<?= base_url('auth/login'); ?>" style="margin-right: 0; padding-right: 0;">Login</a>
+					<a class="nav-link nav_link_menu" href="<?= site_url('auth/register'); ?>">Register</a>
+					<a class="nav-link nav_link_menu" href="<?= site_url('auth/login'); ?>" style="margin-right: 0; padding-right: 0;">Login</a>
 				<?php endif; ?>
 			</ul>
 		</div>
