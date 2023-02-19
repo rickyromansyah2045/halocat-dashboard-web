6fd44e9683b9d7c8189a0e602f085248c3ef9076
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-20 05:15:43 +0700

    update favicon

--- application/views/admin/components/head.php
+++ application/views/admin/components/head.php
@@ -9,7 +9,7 @@
     <link href="<?= base_url('assets/css/custom.css'); ?>" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" rel="stylesheet" crossorigin="anonymous">
-    <link href="<?= base_url('assets/assets/img/favicon.png'); ?>" rel="icon" type="image/x-icon">
+    <link href="<?= base_url('assets/favicon.ico'); ?>" rel="shortcut icon" type="image/x-icon">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" data-search-pseudo-elements defer crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js" crossorigin="anonymous"></script>
 </head>
--- application/views/template/head.php
+++ application/views/template/head.php
@@ -13,5 +13,6 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?= base_url('assets/custom-style.css')?>">
 	<title><?= $title; ?></title>
+	<link rel="shortcut icon" href="<?= base_url('assets/favicon.ico'); ?>" type="image/x-icon">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" data-search-pseudo-elements defer crossorigin="anonymous"></script>
 </head>
