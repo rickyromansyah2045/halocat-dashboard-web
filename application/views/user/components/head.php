dd715b352aad974ecae72ff1a2b2de30835ede0d
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-20 05:19:30 +0700

    change favicon position

--- application/views/user/components/head.php
+++ application/views/user/components/head.php
@@ -5,10 +5,10 @@
     <meta name="description" content="Set Aside Your Money to Help Others Who Are In Need! - The Cloud Donation">
     <meta name="author" content="M Saleh Solahudin, Ricky Romansyah, M Rifki Hikmawan">
     <title>User Area - <?= $title ?> - The Cloud</title>
+    <link href="<?= base_url('assets/favicon.ico'); ?>" rel="shortcut icon" type="image/x-icon">
     <link href="<?= base_url('assets/css/styles.css'); ?>" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" rel="stylesheet" crossorigin="anonymous">
-    <link href="<?= base_url('assets/favicon.ico'); ?>" rel="shortcut icon" type="image/x-icon">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" data-search-pseudo-elements defer crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js" crossorigin="anonymous"></script>
 </head>
