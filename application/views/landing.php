9a3c1f7218417b4c845a9b000b4a4227274bbc15
Muhamad Rifki Hikmawan <rifki@braincodesolution.com>
2023-02-19 12:49:58 +0700

    update landing page

--- application/views/landing.php
+++ application/views/landing.php
@@ -217,6 +217,88 @@
 				</div>
 			</div>
 		</div>
+		<div class="container" style="margin-top: 100px;">
+			<section class="head_review" style="text-align: center;">
+				<p style="font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 35px;
+    line-height: 52px;
+    text-align: center;
+    color: #222222;
+    margin-bottom: 0px;">Heartwarming Stories</p>
+				<p style="    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 30px;
+    color: #222222;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+			</section>
+			<section class="slider_review">
+			
+			</section>
+			<section class="box_faq">
+				<p style="font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 35px;
+    line-height: 52px;
+    text-align: center;
+    color: #222222;
+    margin-bottom: 0px;">Frequently Asked Questions</p>
+				<div class="box_list_faq" style="width: 100%;
+    height: 80px;
+    background: #F5F5F5;
+    border: 1px solid #000000;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    padding: 0 40px;margin-bottom: 5px;">
+					<p style="font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 25px;
+    line-height: 38px;
+    color: #000000;
+    margin: 0px;
+    text-transform: uppercase;">faq</p>
+				</div>
+				<div class="box_list_faq" style="width: 100%;
+    height: 80px;
+    background: #F5F5F5;
+    border: 1px solid #000000;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    padding: 0 40px;margin-bottom: 5px;">
+					<p style="font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 25px;
+    line-height: 38px;
+    color: #000000;
+    margin: 0px;
+    text-transform: uppercase;">faq</p>
+				</div>
+				<div class="box_list_faq" style="width: 100%;
+    height: 80px;
+    background: #F5F5F5;
+    border: 1px solid #000000;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    padding: 0 40px;margin-bottom: 5px;">
+					<p style="font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 25px;
+    line-height: 38px;
+    color: #000000;
+    margin: 0px;
+    text-transform: uppercase;">faq</p>
+				</div>
+			</section>
+		</div>
 		<?php $this->load->view('template/landing_footer'); ?>
 		<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
--- application/views/template/landing_footer.php
+++ application/views/template/landing_footer.php
@@ -1,2 +1,35 @@
 <footer class="footer_home">
+    <div class="container" style="padding: 50px 15px;">
+        <div class="row">
+            <div class="col-md-6">
+                <div style="display: flex;align-items: center;">
+                    <a class="navbar-brand" href="#"></a>
+                    <p style="font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 26px;
+    line-height: 39px;
+    color: #FFFFFF;
+    margin: 0px;padding: 0px 40px;     text-transform: capitalize;">backer inc.</p>
+                </div>
+                <p>Helps people execute their bright ideas</p>
+            </div>
+            <div class="col-md-3" style="padding:20px 0px;">
+                <p style="font-family: 'Poppins';
+font-style: normal;
+font-weight: 600;
+font-size: 18px;
+line-height: 27px;
+color: #FFFFFF;">Jelajahi</p>
+            </div>
+            <div class="col-md-3" style="padding:20px 0px;">
+                <p style="font-family: 'Poppins';
+font-style: normal;
+font-weight: 600;
+font-size: 18px;
+line-height: 27px;
+color: #FFFFFF;">Office</p>
+            </div>
+        </div>
+    </div>
 </footer>
