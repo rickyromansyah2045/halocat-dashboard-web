096b5a15b48e9071fda3abb3f438a788ed111f04
RomansyahOfficial <ricky@hotelmurah.com>
2023-02-19 15:50:42 +0700

    Update user setting value input

--- application/views/user/settings/index_script.php
+++ application/views/user/settings/index_script.php
@@ -1,4 +1,35 @@
 <script>
+
+    function verifikasi_data() {
+        var name = $('#name').val();
+        var email = $('#email').val();
+        var password = $('#password').val();
+
+        if (email != "" && password != "" && name != "") {
+            proses_update(name, email, password);
+        } else {
+            if (name == "") {
+                Swal.fire({
+                    icon: 'error',
+                    text: ("Name Cannot Be Empty")
+                });
+            }
+            if (email == "") {
+                Swal.fire({
+                    icon: 'error',
+                    text: ("Email Cannot Be Empty")
+                });
+            }
+            if (password == "") {
+                Swal.fire({
+                    icon: 'error',
+                    text: ("Password Cannot Be Empty")
+                });
+            }
+        }
+    }
+
+
     var tabel;
 
     $(document).ready(function() {
