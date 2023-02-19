fc7f6845d236fbd01dbbff8b96f04d692812aeb7
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-19 20:23:49 +0700

    change home image hero

--- application/views/landing.php
+++ application/views/landing.php
@@ -19,7 +19,7 @@
 							<button class="btn_header">take me to goodness</button>
 						</div>
 						<div class="col-md-6 padl45">
-							<img src="<?= base_url() ?>/assets/test-img.png" style="" class="img_landing_page">
+							<img src="https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" style="" class="img_landing_page">
 						</div>
 					</div>
 				</div>
