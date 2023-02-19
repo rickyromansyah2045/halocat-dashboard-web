bce092c60d0677c02fa3d956020a46dbb659c07c
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-20 04:31:33 +0700

    adding shadow to navbar

--- application/views/landing.php
+++ application/views/landing.php
@@ -426,6 +426,14 @@
 					}
 				});
 			});
+
+			$(window).scroll(function(){
+				if ($(window).scrollTop() > 10) {
+					$('nav').addClass('navbar-shadow');
+				} else {
+					$('nav').removeClass('navbar-shadow');
+				}
+			});
 		</script>
 	</body>
 </html>
