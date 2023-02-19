c4b11f44d52967a6aad0568fa93bd613ddb9366a
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-19 21:04:38 +0700

    update about and team

--- application/views/about.php
+++ application/views/about.php
@@ -10,24 +10,26 @@
 		</header>
 		<div class="container paddingt100">
 			<div class="row">
-				<div class="col-md-5" style="max-width: 400px;margin-right: 55px;">
-					<img src="<?= base_url('assets/test-img.png')?>" class="img_landing_page">
+				<div class="col-md-5">
+					<img src="https://i.pinimg.com/564x/75/78/c5/7578c533f1fef9d90d2e8eade020484b.jpg" style="max-width: 100%; border-radius: 10px;">
 				</div>
 				<div class="col-md-7">
 					<p style="margin-bottom: 18px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 400;font-size: 15px;line-height: 125%;text-align: justify;color: #000000;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et dui eget felis tincidunt lacinia sit amet non orci. Nullam consectetur velit sed sodales malesuada. Donec nec elementum magna, lacinia feugiat turpis. Morbi vitae posuere turpis. Cras sed nisl pretium, auctor est vitae, egestas erat. Duis imperdiet magna ac tellus aliquet, sed ultricies leo varius. Phasellus porta accumsan posuere. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur molestie varius blandit. Pellentesque et nunc laoreet, pulvinar ipsum et.</p>
 					<p style="margin-bottom: 18px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 400;font-size: 15px;line-height: 125%;text-align: justify;color: #000000;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et dui eget felis tincidunt lacinia sit amet non orci. Nullam consectetur velit sed sodales malesuada. Donec nec elementum magna, lacinia feugiat turpis. Morbi vitae posuere turpis. Cras sed nisl pretium, auctor est vitae, egestas erat. Duis imperdiet magna ac tellus aliquet, sed ultricies leo varius. Phasellus porta accumsan posuere. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur molestie varius blandit. Pellentesque et nunc laoreet, pulvinar ipsum et.</p>
-					<p style="margin-bottom: 18px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 400;font-size: 15px;line-height: 125%;text-align: justify;color: #000000;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et dui eget felis tincidunt lacinia sit amet non orci. Nullam consectetur velit sed sodales malesuada. Donec nec elementum magna, lacinia feugiat turpis. Morbi vitae posuere turpis. Cras sed nisl pretium, auctor est vitae, egestas erat. Duis imperdiet magna ac tellus aliquet, sed ultricies leo varius. Phasellus porta accumsan posuere. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur molestie varius blandit. Pellentesque et nunc laoreet, pulvinar ipsum et.</p>
+					<p style="margin-bottom: 18px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 400;font-size: 15px;line-height: 125%;text-align: justify;color: #000000;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et dui eget felis tincidunt lacinia sit amet non orci.</p>
 				</div>
 			</div>
 			<div class="headline_donasi" style="margin-top: 100px;height: 83px;padding: 0px;">
 				<p class="txt_list_donasi">Meet Our Team</p>
-				<span class="txt_sub_list_donasi">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+				<span class="txt_sub_list_donasi">
+					Meet our professional team, our team is very solid and strong
+				</span>
 			</div>
 			<div class="list_donasi" style="margin-top: 41px;">
 				<div class="row" style="">
 					<div class="col-6 col-md-4 col-lg-3" style="margin-bottom: 30px;">
 						<div class="card" style="border: unset;border-radius: unset;align-items: center;">
-							<img class="card-img-top" src="<?= base_url('assets/Image-list-donasi.svg')?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 20px;width: 225px;height: 225px;object-fit: cover;">
+							<img class="card-img-top" src="<?= base_url('assets/img/saleh.webp')?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 10px;width: 225px;height: 225px;object-fit: cover;">
 							<div class="card-body" style="padding:10px">
 							  	<p style="margin-bottom: 3px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 700;font-size: 20px;line-height: 125%;text-align: center;color: #000000;">M Saleh Solahudin</p>
 							  	<p style="margin-bottom: 0px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 400;font-size: 17px;line-height: 125%;text-align: center;color: #000000;">Full Stack Engineer</p>
@@ -36,7 +38,7 @@
 					</div>
 					<div class="col-6 col-md-4 col-lg-3" style="margin-bottom: 30px;">
 						<div class="card" style="border: unset;border-radius: unset;align-items: center;">
-							<img class="card-img-top" src="<?= base_url('assets/Image-list-donasi.svg')?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 20px;width: 225px;height: 225px;object-fit: cover;">
+							<img class="card-img-top" src="<?= base_url('assets/img/ricky.jpg')?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 10px;width: 225px;height: 225px;object-fit: cover;">
 							<div class="card-body" style="padding:10px">
 							  	<p style="margin-bottom: 3px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 700;font-size: 20px;line-height: 125%;text-align: center;color: #000000;">Ricky Romansyah</p>
 							  	<p style="margin-bottom: 0px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 400;font-size: 17px;line-height: 125%;text-align: center;color: #000000;">Full Stack Engineer</p>
@@ -45,7 +47,7 @@
 					</div>
 					<div class="col-6 col-md-4 col-lg-3" style="margin-bottom: 30px;">
 						<div class="card" style="border: unset;border-radius: unset;align-items: center;">
-							<img class="card-img-top" src="<?= base_url('assets/Image-list-donasi.svg')?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 20px;width: 225px;height: 225px;object-fit: cover;">
+							<img class="card-img-top" src="<?= base_url('assets/img/default_image.png')?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 10px;width: 225px;height: 225px;object-fit: cover;">
 							<div class="card-body" style="padding:10px">
 							  	<p style="margin-bottom: 3px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 700;font-size: 20px;line-height: 125%;text-align: center;color: #000000;">M Rifki Hikmawan</p>
 							  	<p style="margin-bottom: 0px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 400;font-size: 17px;line-height: 125%;text-align: center;color: #000000;">Front-End Engineer</p>
@@ -54,7 +56,7 @@
 					</div>
 					<div class="col-6 col-md-4 col-lg-3" style="margin-bottom: 30px;">
 						<div class="card" style="border: unset;border-radius: unset;align-items: center;">
-							<img class="card-img-top" src="<?= base_url('assets/Image-list-donasi.svg')?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 20px;width: 225px;height: 225px;object-fit: cover;">
+							<img class="card-img-top" src="<?= base_url('assets/img/default_image.png')?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 10px;width: 225px;height: 225px;object-fit: cover;">
 							<div class="card-body" style="padding:10px">
 							  	<p style="margin-bottom: 3px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 700;font-size: 20px;line-height: 125%;text-align: center;color: #000000;">John Doe</p>
 							  	<p style="margin-bottom: 0px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 400;font-size: 17px;line-height: 125%;text-align: center;color: #000000;">Anonymous</p>
@@ -63,7 +65,7 @@
 					</div>
 					<div class="col-6 col-md-4 col-lg-3" style="margin-bottom: 30px;">
 						<div class="card" style="border: unset;border-radius: unset;align-items: center;">
-							<img class="card-img-top" src="<?= base_url('assets/Image-list-donasi.svg')?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 20px;width: 225px;height: 225px;object-fit: cover;">
+							<img class="card-img-top" src="<?= base_url('assets/img/default_image.png') ?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 10px;width: 225px;height: 225px;object-fit: cover;">
 							<div class="card-body" style="padding:10px">
 							  	<p style="margin-bottom: 3px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 700;font-size: 20px;line-height: 125%;text-align: center;color: #000000;">Jane Doe</p>
 							  	<p style="margin-bottom: 0px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 400;font-size: 17px;line-height: 125%;text-align: center;color: #000000;">Anonymous</p>
@@ -72,7 +74,7 @@
 					</div>
 					<div class="col-6 col-md-4 col-lg-3" style="margin-bottom: 30px;">
 						<div class="card" style="border: unset;border-radius: unset;align-items: center;">
-							<img class="card-img-top" src="<?= base_url('assets/Image-list-donasi.svg')?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 20px;width: 225px;height: 225px;object-fit: cover;">
+							<img class="card-img-top" src="<?= base_url('assets/img/default_image.png') ?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 10px;width: 225px;height: 225px;object-fit: cover;">
 							<div class="card-body" style="padding:10px">
 							  	<p style="margin-bottom: 3px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 700;font-size: 20px;line-height: 125%;text-align: center;color: #000000;">Doe John</p>
 							  	<p style="margin-bottom: 0px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 400;font-size: 17px;line-height: 125%;text-align: center;color: #000000;">Anonymous</p>
@@ -81,7 +83,7 @@
 					</div>
 					<div class="col-6 col-md-4 col-lg-3" style="margin-bottom: 30px;">
 						<div class="card" style="border: unset;border-radius: unset;align-items: center;">
-							<img class="card-img-top" src="<?= base_url('assets/Image-list-donasi.svg')?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 20px;width: 225px;height: 225px;object-fit: cover;">
+							<img class="card-img-top" src="<?= base_url('assets/img/default_image.png') ?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 10px;width: 225px;height: 225px;object-fit: cover;">
 							<div class="card-body" style="padding:10px">
 							  	<p style="margin-bottom: 3px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 700;font-size: 20px;line-height: 125%;text-align: center;color: #000000;">Doe Jane</p>
 							  	<p style="margin-bottom: 0px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 400;font-size: 17px;line-height: 125%;text-align: center;color: #000000;">Anonymous</p>
@@ -90,7 +92,7 @@
 					</div>
 					<div class="col-6 col-md-4 col-lg-3" style="margin-bottom: 30px;">
 						<div class="card" style="border: unset;border-radius: unset;align-items: center;">
-							<img class="card-img-top" src="<?= base_url('assets/Image-list-donasi.svg')?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 20px;width: 225px;height: 225px;object-fit: cover;">
+							<img class="card-img-top" src="<?= base_url('assets/img/default_image.png') ?>" alt="Card image cap" style="background: #F5F5F5;border: 1px solid #000000;border-radius: 10px;width: 225px;height: 225px;object-fit: cover;">
 							<div class="card-body" style="padding:10px">
 							  	<p style="margin-bottom: 3px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 700;font-size: 20px;line-height: 125%;text-align: center;color: #000000;">Jane John</p>
 							  	<p style="margin-bottom: 0px;font-family: 'Source Sans Pro';font-style: normal;font-weight: 400;font-size: 17px;line-height: 125%;text-align: center;color: #000000;">Anonymous</p>
