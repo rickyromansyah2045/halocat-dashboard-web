0268ae39f7f6e89c9f9e53d7279229773a1969fc
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-20 05:16:21 +0700

    add favicon to auth pages

--- application/views/auth/header_auth.php
+++ application/views/auth/header_auth.php
@@ -11,6 +11,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="<?= base_url('assets/css/sb-admin-2.min.css'); ?>" rel="stylesheet">
     <link href="<?= base_url('assets/auth.css')?>" rel="stylesheet">
+    <link href="<?= base_url('assets/favicon.ico'); ?>" rel="shortcut icon" type="image/x-icon">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 <body class="bg-gradient-primary">
