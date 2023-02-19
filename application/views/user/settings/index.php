1862abb5eeb7d9b1e7679aa85cf6f47f27728eea
RomansyahOfficial <ricky@hotelmurah.com>
2023-02-19 15:25:51 +0700

    Ufdate user setting

--- application/views/user/settings/index.php
+++ application/views/user/settings/index.php
@@ -19,13 +19,53 @@
         <div class="card-header">Update Your Account</div>
         <div class="card-body">
             <div class="col-10">
-                <form>
-                    <div class="form-group"><label for="name">Name</label><input class="form-control" id="name" type="text" placeholder="Ricky Romansyah"></div>
+                    <div class="form-group"><label for="firstname">First Name</label><input class="form-control" id="firstname" type="text" placeholder="Ricky"></div>
+                    <div class="form-group"><label for="lastname">Last Name</label><input class="form-control" id="lastname" type="text" placeholder="Romansyah"></div>
                     <div class="form-group"><label for="email">Email Address</label><input class="form-control" id="email" type="email" placeholder="name@example.com"></div>
                     <div class="form-group"><label for="password">Password</label><input class="form-control" id="password" type="password" placeholder="**********"></div>
-                    <button class="btn btn-primary" type="button">Update</button>
-                </form>
+                    <button tipe="submit" onclick="verifikasi_data()" class="btn btn-primary">Update</button>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+
+function verifikasi_data() {
+        var firstname = $('#firstname').val();
+        var lastname = $('#lastname').val();
+        var email = $('#email').val();
+        var password = $('#password').val();
+
+        if (email != "" && password != "" && firstname != "" && lastname != "") {
+            proses_update(firstname, lastname, email, password);
+        } else {
+            if (firstname == "") {
+                Swal.fire({
+                    icon: 'error',
+                    text: ("First Name Tidak Boleh Kosong")
+                });
+            }
+            if (lastname == "") {
+                Swal.fire({
+                    icon: 'error',
+                    text: ("Last Name Tidak Boleh Kosong")
+                });
+            }
+            if (email == "") {
+                Swal.fire({
+                    icon: 'error',
+                    text: ("Email Tidak Boleh Kosong")
+                });
+            }
+            if (password == "") {
+                Swal.fire({
+                    icon: 'error',
+                    text: ("Password Tidak Boleh Kosong")
+                });
+            }
+        }
+    }
+
+
+</script>
