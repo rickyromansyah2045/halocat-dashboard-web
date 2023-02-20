d737bf571d4c53f10f12c8ea3de4ef622b79c4e7
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-20 21:46:50 +0700

    modal view more transaction

--- application/views/user/transactions/index.php
+++ application/views/user/transactions/index.php
@@ -37,3 +37,49 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="modal-view-more" tabindex="-1" role="dialog" aria-labelledby="modal-view-more-label" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">View More Data Transaction</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>
+                    <b>Campaign:</b><br>
+                    <span id="view-more-campaign"></span>
+                </p>
+                <p>
+                    <b>Amount:</b><br>
+                    <span id="view-more-amount"></span>
+                </p>
+                <p>
+                    <b>Status:</b><br>
+                    <span id="view-more-status" class="text-capitalize"></span>
+                </p>
+                <p>
+                    <b>Code:</b><br>
+                    <span id="view-more-code"></span>
+                </p>
+                <p>
+                    <b>Comment:</b><br>
+                    <span id="view-more-comment"></span>
+                </p>
+                <p>
+                    <b>Payment URL:</b><br>
+                    <span id="view-more-payment_url"></span>
+                </p>
+                <p class="mb-0">
+                    <b>Payment Token:</b><br>
+                    <span id="view-more-payment_token"></span>
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-primary" type="button" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
