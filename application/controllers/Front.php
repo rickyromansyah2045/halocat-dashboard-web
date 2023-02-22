9ef1a62b3bddc11fd326e68f04f5343e71a1ff4a
RomansyahOfficial <ricky@hotelmurah.com>
2023-02-22 16:44:38 +0700

    updated FE

--- application/controllers/Front.php
+++ application/controllers/Front.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Front extends CI_Controller {
 
+	public function __construct()
+    {
+        parent::__construct();
+		$this->load->helper('x_helper');
+	}
+
 	public function index()
 	{
 		$data['title'] = 'Home';
