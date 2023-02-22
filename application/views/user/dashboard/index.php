759ea5d6551458e595ca0b18323839bf2b99c19f
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-22 14:50:56 +0700

    update responsive dashboard

--- application/views/user/dashboard/index.php
+++ application/views/user/dashboard/index.php
@@ -19,7 +19,7 @@
                 <div class="card-body h-100 d-flex flex-column justify-content-center py-5 py-xl-4">
                     <div class="row align-items-center">
                         <div class="col-12">
-                            <div class="text-center text-xl-left text-xxl-center px-4 mb-4 mb-xl-0 mb-xxl-4">
+                            <div class="px-4 mb-4 mb-xl-0 mb-xxl-4">
                                 <h1 class="text-primary">Welcome to The Cloud Donations!</h1>
                                 <p class="text-gray-700 mb-2">Set Aside Your Money to Help Others Who Are In Need!</p>
                                 <hr>
