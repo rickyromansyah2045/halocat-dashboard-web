b932fce355f980d872911339fea61a080186445d
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-22 07:01:16 +0700

    new request withdrawal format

--- application/views/admin/wallet/index_script.php
+++ application/views/admin/wallet/index_script.php
@@ -1,4 +1,7 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js" integrity="sha512-E8QSvWZ0eCLGk4km3hxSsNmGWbLtSCSUcewDQPQWZF6pEU8GlT8a5fF32wOl1i8ftdMhssTrF/OhyGWwonTcXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
+    var userData;
+
     $(function(){
         $.ajax({
             url: `<?= $_ENV['API_URL']; ?>/users/data`,
@@ -8,6 +11,7 @@
             },
             success: function(response) {
                 if (response.success) {
+                    userData = response?.data;
                     $("#balance").html(response?.data?.e_money || 0);
                     if ((response?.data?.e_money || 0) > 0) {
                         $('#card-body-withdrawal').html(`<form id="form-withdraw"><div class="form-group">
@@ -68,7 +72,11 @@ Account Name: Muhammad Saleh Solahudin"></textarea>
                 return
             }
 
+            let code = CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(JSON.stringify(userData)));
             let text = encodeURIComponent(`[REQUEST WITHDRAWAL]
+
+Request Code:
+${code}
 
 Amount:
 ${$('#amount').val()}
