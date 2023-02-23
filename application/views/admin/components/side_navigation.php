92586dd300ed9a898c32a7fdc35bddf3bcb39d2b
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-23 21:20:39 +0700

    adding front page menu

--- application/views/admin/components/side_navigation.php
+++ application/views/admin/components/side_navigation.php
@@ -3,17 +3,17 @@
         <div class="sidenav-menu">
             <div class="nav accordion" id="accordionSidenav">
                 <div class="sidenav-menu-heading">Main Menu</div>
-                <a class="nav-link" href="<?= base_url('admin/dashboard'); ?>">
+                <a class="nav-link" href="<?= site_url('admin/dashboard'); ?>">
                     <div class="nav-link-icon"><i data-feather="home"></i></div>
                     Dashboard
                 </a>
-                <a class="nav-link" href="<?= base_url('admin/users'); ?>">
+                <a class="nav-link" href="<?= site_url('admin/users'); ?>">
                     <div class="nav-link-icon">
                         <i data-feather="users"></i>
                     </div>
                     Users
                 </a>
-                <a class="nav-link" href="<?= base_url('admin/categories'); ?>">
+                <a class="nav-link" href="<?= site_url('admin/categories'); ?>">
                     <div class="nav-link-icon">
                         <i data-feather="tag"></i>
                     </div>
@@ -25,21 +25,34 @@
                     </div>
                     Campaigns
                 </a>
-                <a class="nav-link" href="<?= base_url('admin/transactions'); ?>">
+                <a class="nav-link" href="<?= site_url('admin/transactions'); ?>">
                     <div class="nav-link-icon"><i data-feather="dollar-sign"></i></div>
                     Transactions
                 </a>
-                <a class="nav-link" href="<?= base_url('admin/logs/activity'); ?>">
+                <a class="nav-link" href="<?= site_url('admin/logs/activity'); ?>">
                     <div class="nav-link-icon"><i data-feather="activity"></i></div>
                     Activity Logs
                 </a>
-                <a class="nav-link" href="<?= base_url('admin/settings'); ?>">
+                <a class="nav-link" href="<?= site_url('admin/settings'); ?>">
                     <div class="nav-link-icon"><i data-feather="settings"></i></div>
                     Account Setting
                 </a>
-                <a class="nav-link" href="<?= base_url('admin/wallet'); ?>">
+                <a class="nav-link" href="<?= site_url('admin/wallet'); ?>">
                     <div class="nav-link-icon"><i data-feather="pocket"></i></div>
                     Wallet Management
+                </a>
+                <div class="sidenav-menu-heading">Front Page Menu</div>
+                <a class="nav-link" href="<?= site_url(); ?>">
+                    <div class="nav-link-icon"><i data-feather="home"></i></div>
+                    Home Page
+                </a>
+                <a class="nav-link" href="<?= site_url('donations'); ?>">
+                    <div class="nav-link-icon"><i data-feather="gift"></i></div>
+                    List of Donations
+                </a>
+                <a class="nav-link" href="<?= site_url('about'); ?>">
+                    <div class="nav-link-icon"><i data-feather="info"></i></div>
+                    About Us
                 </a>
             </div>
         </div>
