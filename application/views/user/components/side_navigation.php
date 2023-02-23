48f1d346d997e831631c1eba23d37075d53b87df
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-23 21:21:14 +0700

    adding front page menu

--- application/views/user/components/side_navigation.php
+++ application/views/user/components/side_navigation.php
@@ -3,27 +3,40 @@
         <div class="sidenav-menu">
             <div class="nav accordion" id="accordionSidenav">
                 <div class="sidenav-menu-heading">Main Menu</div>
-                <a class="nav-link" href="<?= base_url('user/dashboard'); ?>">
+                <a class="nav-link" href="<?= site_url('user/dashboard'); ?>">
                     <div class="nav-link-icon"><i data-feather="home"></i></div>
                     Dashboard
                 </a>
-                <a class="nav-link" href="<?= base_url('user/campaigns'); ?>">
+                <a class="nav-link" href="<?= site_url('user/campaigns'); ?>">
                     <div class="nav-link-icon">
                         <i data-feather="gift"></i>
                     </div>
                     Campaigns
                 </a>
-                <a class="nav-link" href="<?= base_url('user/transactions'); ?>">
+                <a class="nav-link" href="<?= site_url('user/transactions'); ?>">
                     <div class="nav-link-icon"><i data-feather="dollar-sign"></i></div>
                     Transactions
                 </a>
-                <a class="nav-link" href="<?= base_url('user/settings'); ?>">
+                <a class="nav-link" href="<?= site_url('user/settings'); ?>">
                     <div class="nav-link-icon"><i data-feather="settings"></i></div>
                     Account Setting
                 </a>
-                <a class="nav-link" href="<?= base_url('user/wallet'); ?>">
+                <a class="nav-link" href="<?= site_url('user/wallet'); ?>">
                     <div class="nav-link-icon"><i data-feather="pocket"></i></div>
                     Wallet Management
+                </a>
+                <div class="sidenav-menu-heading">Front Page Menu</div>
+                <a class="nav-link" href="<?= site_url(); ?>">
+                    <div class="nav-link-icon"><i data-feather="home"></i></div>
+                    Home Page
+                </a>
+                <a class="nav-link" href="<?= site_url('donations'); ?>">
+                    <div class="nav-link-icon"><i data-feather="gift"></i></div>
+                    List of Donations
+                </a>
+                <a class="nav-link" href="<?= site_url('about'); ?>">
+                    <div class="nav-link-icon"><i data-feather="info"></i></div>
+                    About Us
                 </a>
             </div>
         </div>
