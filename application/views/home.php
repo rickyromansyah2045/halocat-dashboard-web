f0391f033e3b9e9aa562302fc508d520fe3c1034
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-23 07:13:50 +0700

    remove comment

--- application/views/home.php
+++ application/views/home.php
@@ -90,7 +90,6 @@
 			</section>
 			<section class="slider_review mb50">
 				<div id="demo" class="carousel slide" data-ride="carousel">
-					<!-- The slideshow -->
 					<div class="carousel-inner">
 						<div class="carousel-item active">
 							<div class="box_review">
@@ -110,7 +109,7 @@ ut lacinia sapien  porttitor ac. Praesent vel orci lectus. Etiam et nulla odio.<
 										</div>
 									</div>
 									<div class="box_review_right">
-										
+
 									</div>
 								</div>
 							</div>
@@ -131,7 +130,7 @@ ut lacinia sapien  porttitor ac. Praesent vel orci lectus. Etiam et nulla odio.<
 										</div>
 									</div>
 									<div class="box_review_right">
-										
+
 									</div>
 								</div>
 							</div>
@@ -151,7 +150,7 @@ ut lacinia sapien  porttitor ac. Praesent vel orci lectus. Etiam et nulla odio.<
 										</div>
 									</div>
 									<div class="box_review_right">
-										
+
 									</div>
 								</div>
 							</div>
@@ -171,14 +170,12 @@ ut lacinia sapien  porttitor ac. Praesent vel orci lectus. Etiam et nulla odio.<
 										</div>
 									</div>
 									<div class="box_review_right">
-										
+
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-
-					<!-- Left and right controls -->
 					<a class="carousel-control-prev" data-target="#demo" data-slide="prev" style="color: black;width: auto;">
 						<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
 						 	<path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
