f7b599e7e17a51a5d09f382fc89925bc29214d51
RomansyahOfficial <ricky@hotelmurah.com>
2023-02-23 23:58:14 +0700

    Update css home

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -18,3 +18,96 @@
 	<link rel="shortcut icon" href="<?= base_url('assets/favicon.ico'); ?>" type="image/x-icon">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" data-search-pseudo-elements defer crossorigin="anonymous"></script>
 </head>
+<style>
+	/* CSS untuk perangkat mobile home */
+	@media (max-width: 767px) {
+
+		/* Sesuaikan style header */
+		.jumbotron_header {
+			padding-top: 100px !important;
+			text-align: center;
+		}
+
+		.logo_brand {
+			margin-left: 20px;
+		}
+
+		.navbar-toggler {
+			margin-right: 10px;
+		}
+
+		.header_box {
+			height: 550px;
+		}
+
+		.headline_donasi {
+			text-align: center;
+			margin-bottom: -20px;
+		}
+
+		.btn_header{
+			width: 190px;
+		}
+		.padl45 {
+			visibility: hidden;
+		}
+
+		.txt_header1 {
+			font-size: 22px !important;
+		}
+
+		.txt_header2 {
+			font-size: 15px !important;
+		}
+
+		/* Sesuaikan style list donation */
+		.list_donasi {
+			margin-top: 40px !important;
+		}
+
+		.btn_see_all_donasi {
+			font-size: 14px !important;
+		}
+
+		/* Sesuaikan style jumbotron ringkasan donasi */
+		.jumbotron_ringkasan_donasi {
+			padding: 50px 0 !important;
+		}
+
+		.txt_judul_ringkas_donasi1 {
+			font-size: 24px !important;
+		}
+
+		.txt_judul_ringkas_donasi2 {
+			font-size: 12px !important;
+		}
+
+		/* Sesuaikan style heartwarming stories */
+		.headline_judul {
+			font-size: 24px !important;
+		}
+
+		.sub_headline_judul {
+			font-size: 16px !important;
+		}
+
+		.box_review  {
+			width: 250px;
+			height: 300px;
+		}
+
+		.txt_judul_review {
+			font-size: 15px;
+		}
+
+		.box_review_right {
+			width: auto;
+			/* height: 200px; */
+			visibility: hidden;
+		}
+
+		.txt_description_review {
+			font-size: 15px;
+		}
+	}
+</style>
