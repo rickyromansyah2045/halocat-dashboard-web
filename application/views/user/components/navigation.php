8629e84696778f1d6924dd9899d871cfad4b16bb
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-24 20:57:31 +0700

    lazyload img brand on nav and footer

--- application/views/user/components/navigation.php
+++ application/views/user/components/navigation.php
@@ -3,10 +3,10 @@
     <button class="btn btn-icon btn-transparent-dark order-1 order-lg-0 mr-lg-2" id="sidebarToggle"><i data-feather="menu"></i></button>
     <ul class="navbar-nav align-items-center ml-auto">
         <li class="nav-item dropdown no-caret mr-3 mr-lg-0 dropdown-user">
-            <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownUserImage" href="javascript:void(0);" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img class="img-fluid" src="<?= base_url("assets/img/avatar.svg"); ?>" /></a>
+            <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownUserImage" href="javascript:void(0);" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img class="img-fluid" src="<?= base_url('assets/lazyload.gif'); ?>" data-src="<?= base_url("assets/img/avatar.svg"); ?>" /></a>
             <div class="dropdown-menu dropdown-menu-right border-0 shadow animated--fade-in-up" aria-labelledby="navbarDropdownUserImage">
                 <h6 class="dropdown-header d-flex align-items-center">
-                    <img class="dropdown-user-img" src="<?= base_url("assets/img/avatar.svg"); ?>" />
+                    <img class="dropdown-user-img" src="<?= base_url('assets/lazyload.gif'); ?>" data-src="<?= base_url("assets/img/avatar.svg"); ?>" />
                     <div class="dropdown-user-details">
                         <div class="dropdown-user-details-name"><?= $this->session->userdata('name'); ?></div>
                         <div class="dropdown-user-details-email"><?= $this->session->userdata('email'); ?></div>
