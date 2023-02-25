1f2e11d9cda0ade329c0e2d40b16515dc019e310
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-25 20:37:51 +0700

    change wording

--- application/views/user/dashboard/index_script.php
+++ application/views/user/dashboard/index_script.php
@@ -29,7 +29,7 @@
                             }
 
                             $('#reward-notification').append(`<li>
-                                Congrats! you are the chosen one to get the rewards from the exclusive campaign.
+                                Congrats! you are the chosen (winner) to get the rewards from the exclusive campaign.
                                 <ul class="mb-0">
                                     <li>Link campaign: <a href="<?= site_url('donate'); ?>/${data[i].campaign_id}">click this for go to the campaign link</a>.</li>
                                     <li>Reward: ${reward}.</li>
