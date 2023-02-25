58162fb056334715bc1a48fa1657883db1d19926
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-25 20:13:39 +0700

    implement datatables withdrawal requests

--- application/views/admin/wallet/index_script.php
+++ application/views/admin/wallet/index_script.php
@@ -2,6 +2,7 @@
 <script>
     var userData;
     var tabel;
+    var tabel2;
 
     $(function(){
         tabel = $('#dataTable_theCloud').DataTable({
@@ -73,7 +74,90 @@
             ]
         });
 
-        $('.dataTables_filter input').unbind().bind('keyup',function(e) {
+        $('#dataTable_theCloud_wrapper .dataTables_filter input').unbind().bind('keyup',function(e) {
+            if (e.keyCode == 13 || this.value == '') {
+                if (this.value == '') {
+                    if (!empty) {
+                    tabel.search(this.value).draw();
+                    empty = true;
+                    }
+                }else{
+                    tabel.search(this.value).draw();
+                    empty = false;
+                }
+            }
+        });
+
+        tabel2 = $('#dataTable_theCloud_2').DataTable({
+            order: [],
+            stateSave: false,
+            processing: true,
+            serverSide: true,
+            responsive: true,
+            pageLength: 20,
+            searchDelay: 500,
+            language: {
+                lengthMenu: "_MENU_"
+            },
+            ajax: {
+                url: "<?= $_ENV['API_URL']; ?>/datatables/withdrawal",
+                type: 'GET',
+                beforeSend: function(xhr) {
+                    xhr.setRequestHeader("Authorization", "Bearer <?= $this->session->userdata('token'); ?>");
+                },
+                error: function(xhr, error, code) {
+                    $('#dataTable_theCloud_processing').hide();
+                    $('#dataTable_theCloud > tbody').html(`<tr><td colspan="5" class="dataTables_empty">${(xhr?.responseJSON?.message || `${error}, ${(code == "" ? "internal server error or API is down!" : code)}`)}</td></tr>`);
+                }
+            },
+            columnDefs: [{
+                targets: [0],
+                searchable: false,
+                orderable: false
+            }],
+            lengthMenu: [
+                [10, 20, 50, 100, 200, 500],
+                [10, 20, 50, 100, 200, 500]
+            ],
+            drawCallback: function(_) {
+                $('.dataTables_filter input').attr('placeholder','Type and enter...');
+            },
+            initComplete: function(_, _) {},
+            columns: [
+                {
+                    data: "no",
+                    render: function(data, type, row) {
+                        return data;
+                    }
+                },
+                {
+                    data: "status",
+                    render: function(data, type, row) {
+                        return data;
+                    }
+                },
+                {
+                    data: "amount",
+                    render: function(data, type, row) {
+                        return formatRupiah(data);
+                    }
+                },
+                {
+                    data: "note",
+                    render: function(data, type, row) {
+                        return data;
+                    }
+                },
+                {
+                    data: "created_at",
+                    render: function(data, type, row) {
+                        return data;
+                    }
+                }
+            ]
+        });
+
+        $('#dataTable_theCloud_2_wrapper .dataTables_filter input').unbind().bind('keyup',function(e) {
             if (e.keyCode == 13 || this.value == '') {
                 if (this.value == '') {
                     if (!empty) {
