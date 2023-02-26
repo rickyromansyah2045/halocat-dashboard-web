3d2bd00370eef4dc9e971fd6625d25d6a3bc4c91
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-26 11:55:09 +0700

    implement datatables company cash flow

---
+++ application/views/admin/company_cash_flow/index_script.php
@@ -0,0 +1,88 @@
+<script>
+    var table;
+
+    $(document).ready(function() {
+        table = $('#dataTable_theCloud').DataTable({
+            order: [],
+            stateSave: false,
+            processing: true,
+            serverSide: true,
+            responsive: true,
+            pageLength: 20,
+            searchDelay: 500,
+            language: {
+                lengthMenu: "_MENU_"
+            },
+            ajax: {
+                url: "<?= $_ENV['API_URL']; ?>/admin/datatables/company/cashflow",
+                type: 'GET',
+                beforeSend: function(xhr) {
+                    xhr.setRequestHeader("Authorization", "Bearer <?= $this->session->userdata('token'); ?>");
+                },
+                error: function(xhr, error, code) {
+                    $('#dataTable_theCloud_processing').hide();
+                    $('#dataTable_theCloud > tbody').html(`<tr><td colspan="5" class="dataTables_empty">${(xhr?.responseJSON?.message || `${error}, ${(code == "" ? "internal server error or API is down!" : code)}`)}</td></tr>`);
+                }
+            },
+            columnDefs: [{
+                targets: [0, 4],
+                searchable: false,
+                orderable: false
+            }],
+            lengthMenu: [
+                [10, 20, 50, 100, 200, 500],
+                [10, 20, 50, 100, 200, 500]
+            ],
+            drawCallback: function(_) {
+                $('.dataTables_filter input').attr('placeholder','Type and enter...');
+            },
+            initComplete: function(_, _) {},
+            columns: [
+                {
+                    data: "no",
+                    render: function(data, type, row) {
+                        return data;
+                    }
+                },
+                {
+                    data: "amount",
+                    render: function(data, type, row) {
+                        return formatRupiah(data);
+                    }
+                },
+                {
+                    data: "note",
+                    render: function(data, type, row) {
+                        return data;
+                    }
+                },
+                {
+                    data: "status",
+                    render: function(data, type, row) {
+                        return data;
+                    }
+                },
+                {
+                    data: "id",
+                    render: function(data, type, row) {
+                        return "-";
+                    }
+                }
+            ]
+        });
+
+        $('.dataTables_filter input').unbind().bind('keyup',function(e) {
+            if (e.keyCode == 13 || this.value == '') {
+                if (this.value == '') {
+                    if (!empty) {
+                    table.search(this.value).draw();
+                    empty = true;
+                    }
+                }else{
+                    table.search(this.value).draw();
+                    empty = false;
+                }
+            }
+        });
+    });
+</script>
