7e4ac59b2c3800d7dbb86821d90e2e714282e12d
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-02-26 11:17:34 +0700

    route company cash flow

--- application/config/routes.php
+++ application/config/routes.php
@@ -58,4 +58,5 @@ $route['donations'] = "front/donation";
 $route['donate/(:any)'] = "front/donation_detail/$1";
 
 $route['admin/logs/activity'] = "admin/logs_activity";
+$route['admin/company-cash-flow'] = "admin/company_cash_flow";
 $route['admin/withdrawal-requests'] = "admin/withdrawal_requests";
