9269642b8fb8d8737a594c8f7a5a2c4d28288038
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-03-01 22:32:26 +0700

    ajax for chart website activity

--- application/views/admin/dashboard/index_script.php
+++ application/views/admin/dashboard/index_script.php
@@ -55,5 +55,31 @@
             },
             complete: function() {}
         });
+
+        $.ajax({
+            url: `<?= $_ENV['API_URL']; ?>/admin/dashboard/chart?chart=website_activity`,
+            type: 'GET',
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader("Authorization", "Bearer <?= $this->session->userdata('token'); ?>");
+            },
+            success: function(response) {
+                if (response.success) {
+                    let data = response.data;
+
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        text: response.error
+                    });
+                }
+            },
+            error: function(xhr, error, code) {
+                Swal.fire({
+                    icon: 'error',
+                    text: xhr?.responseJSON?.error || `${error}, ${(code == "" ? "internal server error or API is down!" : code)}`
+                });
+            },
+            complete: function() {}
+        });
     });
 </script>
