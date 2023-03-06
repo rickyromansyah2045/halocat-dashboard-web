408e59bea9ec6b4c66f0039817bd0097e7f7b5a3
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-03-06 20:47:50 +0700

    tidy up

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -3,6 +3,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="Set Aside Your Money to Help Others Who Are In Need! - The Cloud Donation">
     <meta name="author" content="M Saleh Solahudin, Ricky Romansyah, M Rifki Hikmawan">
+	<title><?= $title; ?> - The Cloud Donation</title>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Moul&display=swap">
@@ -14,8 +15,7 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?= base_url('assets/custom-style.css')?>">
-	<title><?= $title; ?> - The Cloud Donation</title>
 	<link rel="shortcut icon" href="<?= base_url('assets/favicon.ico'); ?>" type="image/x-icon">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" data-search-pseudo-elements defer crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="<?= base_url('assets/css/styles_mobile.css')?>">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" data-search-pseudo-elements defer crossorigin="anonymous"></script>
 </head>
