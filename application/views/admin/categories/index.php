2a9156b76d985b09a8b368abacebef4d949ca349
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-03-09 22:49:47 +0700

    change modal size

--- application/views/admin/categories/index.php
+++ application/views/admin/categories/index.php
@@ -65,7 +65,7 @@
 </div>
 
 <div class="modal fade" id="modal-edit" tabindex="-1" role="dialog" aria-labelledby="modal-edit-label" style="display: none;" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Edit Category</h5>
