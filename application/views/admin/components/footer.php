ca76e44ddad2c90451c7091abeab600cc8508951
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-03-12 23:57:54 +0700

    update wording

--- application/views/admin/components/footer.php
+++ application/views/admin/components/footer.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-md-6 small">Copyright &copy; The Cloud <?= date('Y'); ?></div>
             <div class="col-md-6 text-md-right small">
-                Created by Saleh, Ricky and Rifki.
+                Created by Saleh, Ricky and Rifki
             </div>
         </div>
     </div>
